ad11029e4654076324763f77eb6637e2f4bac777
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-01-20 14:35:18 +0530

    new

--- src/main/main.js
+++ src/main/main.js
@@ -5,7 +5,7 @@ const http = require("http");
 const cors = require("cors");
 const path = require("path");
 
-
+const load = require("../route/load")
 const app = express();
 
 app.use(cors({
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(express.urlencoded({ limit: "400mb", extended: false }));
 app.use(express.json({ limit: "400mb" }));
 app.use(express.static(path.join(__dirname, "/public/")));
-// app.use(load);
+app.use(load);
 
 
 module.exports = {app}
